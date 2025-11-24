63777c7b0c4271045ce621f9c83863dedb838487
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-24 17:42:35 +0000

    Added CreateAthleteHandler

---
+++ src/KRAFT.Results.Core/Athletes/CreateAthlete/CreateAthleteHandler.cs
@@ -0,0 +1,30 @@
+﻿using KRAFT.Results.Core.Athletes.AddAthlete;
+
+using Microsoft.Extensions.Logging;
+
+namespace KRAFT.Results.Core.Athletes.CreateAthlete;
+
+internal sealed class CreateAthleteHandler
+{
+    private readonly ILogger<CreateAthleteHandler> logger;
+    private readonly ResultsDbContext dbContext;
+
+    public CreateAthleteHandler(ILogger<CreateAthleteHandler> logger, ResultsDbContext dbContext)
+    {
+        this.logger = logger;
+        this.dbContext = dbContext;
+    }
+
+    public async Task Handle(CreateAthleteCommand command, CancellationToken cancellationToken)
+    {
+        Athlete athlete = new()
+        {
+            FirstName = command.FirstName,
+            LastName = command.LastName,
+        };
+
+        this.dbContext.Athletes.Add(athlete);
+
+        await this.dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
