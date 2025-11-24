2966fd20e920dcb3a04f31643c69b51311cb7079
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-24 17:42:28 +0000

    Added CreateAthleteCommand

---
+++ src/KRAFT.Results.Core/Athletes/CreateAthlete/CreateAthleteCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace KRAFT.Results.Core.Athletes.AddAthlete;
+
+public sealed record class CreateAthleteCommand(
+    string FirstName,
+    string LastName,
+    string Gender,
+    int CountryId,
+    DateOnly? DateOfBirth,
+    int? TeamId,
+    string? ProfileImageFilename);
