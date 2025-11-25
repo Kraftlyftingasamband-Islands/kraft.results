63f48c2c570a40d188eb18b2705c7e93881b24e7
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-25 09:46:55 +0000

    Added IWebApiAssemblyMarker

---
+++ src/KRAFT.Results.WebApi/IWebApiAssemblyMarker.cs
@@ -0,0 +1,7 @@
+﻿namespace KRAFT.Results.WebApi;
+
+#pragma warning disable CA1040 // Avoid empty interfaces
+#pragma warning disable CA1515 // Consider making public types internal
+public interface IWebApiAssemblyMarker;
+#pragma warning restore CA1515 // Consider making public types internal
+#pragma warning restore CA1040 // Avoid empty interfaces
