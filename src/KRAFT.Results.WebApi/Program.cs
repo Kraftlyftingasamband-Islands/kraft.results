9ff87eb8e379a93adc52a9dc37b2b3a7dac9afbb
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-25 11:12:13 +0000

    added health checks

--- src/KRAFT.Results.WebApi/Program.cs
+++ src/KRAFT.Results.WebApi/Program.cs
@@ -2,12 +2,16 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddHealthChecks();
+
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapHealthChecks("/healthz");
 
 app.UseHttpsRedirection();
 
---
+++ tests/KRAFT.Results.WebApi.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+using Shouldly;
+
+namespace KRAFT.Results.WebApi.IntegrationTests;
+
+public sealed class HealthCheckTests : IClassFixture<IntegrationTestFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    public HealthCheckTests(IntegrationTestFactory factory)
+    {
+        ArgumentNullException.ThrowIfNull(factory);
+
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task HealthCheckReturnsHealthyStatus()
+    {
+        // Arrange
+        using HttpRequestMessage request = new(HttpMethod.Get, "/healthz");
+
+        // Act
+        HttpResponseMessage response = await _httpClient.SendAsync(request, CancellationToken.None);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+}
