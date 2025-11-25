07893b45a7db8fa8b53bfdd82227aa1c27b0bc91
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-25 13:03:44 +0000

    suppressed CA1515

--- src/KRAFT.Results.WebApi/ResultsDbContext.cs
+++ src/KRAFT.Results.WebApi/ResultsDbContext.cs
@@ -2,7 +2,9 @@
 
 namespace KRAFT.Results.Core;
 
+#pragma warning disable CA1515 // Consider making public types internal
 public sealed class ResultsDbContext(DbContextOptions<ResultsDbContext> options) : DbContext(options)
+#pragma warning restore CA1515 // Consider making public types internal
 {
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
