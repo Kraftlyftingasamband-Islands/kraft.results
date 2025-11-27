5c931b87d88244d3f9baa6f49d68ecaa7f2cb45c
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-27 15:38:48 +0000

    feat: added global exception handling (#22)

---
+++ src/KRAFT.Results.WebApi/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KRAFT.Results.WebApi.Middleware;
+
+internal sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+    private readonly IProblemDetailsService _problemDetailsService;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IProblemDetailsService problemDetailsService)
+    {
+        _problemDetailsService = problemDetailsService;
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "An unhandled exception occurred.");
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        httpContext.Response.ContentType = "application/problem+json";
+
+        ProblemDetails problemDetails = new()
+        {
+            Type = exception.GetType().Name,
+            Title = "An unexpected error occurred.",
+            Detail = exception.Message,
+            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}",
+        };
+
+        ProblemDetailsContext context = new()
+        {
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = problemDetails,
+        };
+
+        return await _problemDetailsService.TryWriteAsync(context);
+    }
+}
