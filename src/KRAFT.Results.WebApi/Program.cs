f020c119c5c5a883e43b21675a6459bcd7090f82
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-27 15:32:32 +0000

    feat: added global exception handling

--- src/KRAFT.Results.WebApi/Program.cs
+++ src/KRAFT.Results.WebApi/Program.cs
@@ -1,8 +1,12 @@
+using System.Diagnostics;
+
 using KRAFT.Results.WebApi;
 using KRAFT.Results.WebApi.Features.Athletes;
 using KRAFT.Results.WebApi.Features.Teams;
 using KRAFT.Results.WebApi.Features.Users;
+using KRAFT.Results.WebApi.Middleware;
 
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 
 using Scalar.AspNetCore;
@@ -14,8 +18,21 @@ builder.Services.AddDbContext<ResultsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = context =>
+    {
+        Activity? activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+        context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+        context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
+    };
+});
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
+
 builder.Services.AddAthletes();
 builder.Services.AddUsers(builder.Configuration);
 builder.Services.AddTeams();
@@ -29,6 +46,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapHealthChecks("/healthz");
+app.UseExceptionHandler();
+app.UseStatusCodePages();
+
 app.MapAthleteEndpoints();
 app.MapTeamEndpoints();
 app.MapUserEndpoints();
