9cf4042a39fb1978aa5b2a529945deac1aad8867
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-29 11:44:08 +0000

    fix: changed team slug to not nullable

--- src/KRAFT.Results.WebApi/Migrations/20251129113851_Update_Teams_Slug_NotNullable.Designer.cs
+++ src/KRAFT.Results.WebApi/Migrations/20251129113851_Update_Teams_Slug_NotNullable.Designer.cs
@@ -4,6 +4,7 @@ using KRAFT.Results.WebApi;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KRAFT.Results.WebApi.Migrations
 {
     [DbContext(typeof(ResultsDbContext))]
-    partial class ResultsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251129113851_Update_Teams_Slug_NotNullable")]
+    partial class Update_Teams_Slug_NotNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/KRAFT.Results.WebApi/Migrations/20251129113851_Update_Teams_Slug_NotNullable.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KRAFT.Results.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Teams_Slug_NotNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                schema: "dbo",
+                table: "Teams",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                schema: "dbo",
+                table: "Teams",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
