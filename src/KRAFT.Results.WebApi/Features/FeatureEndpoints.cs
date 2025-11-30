9629bfea9dcd78835f8d5e6fa263b37eb842ce14
Kári Rafn Karlsson <karirafn@gmail.com>
2025-11-30 13:38:33 +0000

    refactor: moved feature endpoint mapping to extension method (#76)

---
+++ src/KRAFT.Results.WebApi/Features/FeatureEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using KRAFT.Results.WebApi.Features.Athletes;
+using KRAFT.Results.WebApi.Features.Meets;
+using KRAFT.Results.WebApi.Features.Teams;
+using KRAFT.Results.WebApi.Features.Users;
+
+namespace KRAFT.Results.WebApi.Features;
+
+internal static class FeatureEndpoints
+{
+    internal static IEndpointRouteBuilder MapFeatureEndpoints(this IEndpointRouteBuilder builder)
+    {
+        builder.MapAthleteEndpoints();
+        builder.MapTeamEndpoints();
+        builder.MapUserEndpoints();
+        builder.MapMeetEndpoints();
+
+        return builder;
+    }
+}
