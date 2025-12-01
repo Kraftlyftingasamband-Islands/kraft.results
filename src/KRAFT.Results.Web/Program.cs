8dad7ea8174f3eecf07450dc5d0d349368bfebf7
Kári Rafn Karlsson <karirafn@gmail.com>
2025-12-01 09:47:59 +0000

    feat: added KRAFT.Results.Web

---
+++ src/KRAFT.Results.Web/Program.cs
@@ -0,0 +1,36 @@
+using KRAFT.Results.Web.Components;
+using KRAFT.Results.Web.Services;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<IApiService, ApiService>();
+
+builder.Services.AddHttpClient<IApiService, ApiService>(client =>
+{
+    Uri baseAddress = builder.Configuration.GetValue<Uri>("API:BaseAddress")
+        ?? throw new InvalidOperationException("No API base address");
+
+    client.BaseAddress = baseAddress;
+});
+
+WebApplication app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+await app.RunAsync();
---
+++ src/KRAFT.Results.Web/Services/IApiService.cs
@@ -0,0 +1,6 @@
+﻿namespace KRAFT.Results.Web.Services;
+
+internal interface IApiService
+{
+    Task<T?> GetAsync<T>(string path);
+}
