986db9bc70e69c2d22461150e61c6bd2412af154
Kári Rafn Karlsson <karirafn@gmail.com>
2025-12-01 09:52:36 +0000

    feat: added KRAFT.Results.Web (#81)

---
+++ src/KRAFT.Results.Web/Services/ApiService.cs
@@ -0,0 +1,13 @@
+﻿namespace KRAFT.Results.Web.Services;
+
+internal sealed class ApiService : IApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public ApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public Task<T?> GetAsync<T>(string path) => _httpClient.GetFromJsonAsync<T>(path);
+}
