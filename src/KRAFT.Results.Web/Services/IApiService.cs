66638155fb382dad25c2b99d28283de6688f8c78
Kári Rafn Karlsson <karirafn@gmail.com>
2025-12-02 11:26:02 +0000

    feat: added login page

--- src/KRAFT.Results.Web/Services/IApiService.cs
+++ src/KRAFT.Results.Web/Services/IApiService.cs
@@ -3,4 +3,10 @@
 internal interface IApiService
 {
     Task<T?> GetAsync<T>(string path);
+
+    Task<HttpResponseMessage> PostAsync<T>(string path, T body);
+
+    Task<bool> LoginAsync(string username, string password);
+
+    void Logout();
 }
