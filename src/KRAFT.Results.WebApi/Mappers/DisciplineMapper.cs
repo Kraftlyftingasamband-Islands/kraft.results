7389b5e21c20db5d30b7ab4e4f59bfbf8dd4a9dd
Kári Rafn Karlsson <karirafn@gmail.com>
2025-12-21 21:11:58 +0000

    Added DisciplineMapper

---
+++ src/KRAFT.Results.WebApi/Mappers/DisciplineMapper.cs
@@ -0,0 +1,21 @@
+﻿using KRAFT.Results.Contracts;
+using KRAFT.Results.WebApi.Enums;
+
+namespace KRAFT.Results.WebApi.Mappers;
+
+internal static class DisciplineMapper
+{
+#pragma warning disable S3358 // Ternary operators should not be nested
+    public static string Map(int disciplineId) =>
+        IsSquat(disciplineId) ? Constants.Squat
+        : IsBench(disciplineId) ? Constants.Bench
+        : IsDeadlift(disciplineId) ? Constants.Deadlift
+        : Constants.Total;
+#pragma warning restore S3358 // Ternary operators should not be nested
+
+    private static bool IsSquat(int disciplineId) => disciplineId == (byte)Discipline.Squat;
+
+    private static bool IsBench(int disciplineId) => disciplineId == (byte)Discipline.Bench;
+
+    private static bool IsDeadlift(int disciplineId) => disciplineId == (byte)Discipline.Deadlift;
+}
