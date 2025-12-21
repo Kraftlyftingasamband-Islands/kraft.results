2186d806d013bdebe85a9c09c31e2ff9659bba8e
Kári Rafn Karlsson <karirafn@gmail.com>
2025-12-21 21:14:55 +0000

    Added MeetParticipation DTO

---
+++ src/KRAFT.Results.Contracts/Meets/MeetParticipation.cs
@@ -0,0 +1,15 @@
+﻿namespace KRAFT.Results.Contracts.Meets;
+
+public sealed record class MeetParticipation(
+    int Rank,
+    string Name,
+    string Gender,
+    int YearOfBirth,
+    string AgeCategory,
+    string WeightCategory,
+    string Club,
+    string ClubSlug,
+    decimal BodyWeight,
+    decimal Total,
+    decimal IpfPoints,
+    IEnumerable<MeetAttempt> Attempts);
