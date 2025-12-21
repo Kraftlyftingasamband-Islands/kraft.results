eee6515a8d0348e9e82c844b2c0516ccf6a57bef
Kári Rafn Karlsson <karirafn@gmail.com>
2025-12-21 19:54:33 +0000

    Changed ResultModeId back to int

--- src/KRAFT.Results.WebApi/Features/Meets/Meet.cs
+++ src/KRAFT.Results.WebApi/Features/Meets/Meet.cs
@@ -1,5 +1,4 @@
 ﻿using KRAFT.Results.WebApi.Abstractions;
-using KRAFT.Results.WebApi.Enums;
 using KRAFT.Results.WebApi.Features.Participations;
 using KRAFT.Results.WebApi.Features.Photos;
 using KRAFT.Results.WebApi.Features.Users;
@@ -31,7 +30,7 @@ internal sealed class Meet
 
     public bool PublishedResults { get; private set; }
 
-    public ResultMode ResultModeId { get; private set; }
+    public int ResultModeId { get; private set; }
 
     public bool PublishedInCalendar { get; private set; }
 
