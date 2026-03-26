928a22a90b31760b15afc6a867e6ea94b6bbc9e3
Kári Rafn Karlsson <karirafn@gmail.com>
2026-03-26 12:43:28 +0000

    feat(records): add migration to populate era slugs for existing rows

---
+++ src/KRAFT.Results.WebApi/Migrations/20260326123036_Seed_Era_Slugs.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KRAFT.Results.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed_Era_Slugs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE dbo.Eras SET Slug = 'pre-isi' WHERE Title = 'Pre-ISI' AND Slug IS NULL;");
+            migrationBuilder.Sql("UPDATE dbo.Eras SET Slug = 'isi-old-weightcategories' WHERE Title = 'ISI Old Weightcategories' AND Slug IS NULL;");
+            migrationBuilder.Sql("UPDATE dbo.Eras SET Slug = 'isi-new-weightcategories' WHERE Title = 'ISI New WeightCategories' AND Slug IS NULL;");
+            migrationBuilder.Sql("UPDATE dbo.Eras SET Slug = 'pre-isi-old-weightcategories' WHERE Title = 'PRE-ISI Old WeightCategories' AND Slug IS NULL;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No-op: do not NULL out slugs on rollback to avoid data loss.
+        }
+    }
+}
