bdd054ca307a1b7c8b74e2690a3e88c98067544f
Kári Rafn Karlsson <karirafn@gmail.com>
2026-04-06 13:57:50 +0000

    feat(events): add domain event records and raise from aggregates (#351)

---
+++ src/KRAFT.Results.WebApi/Features/Athletes/AthleteCreatedEvent.cs
@@ -0,0 +1,5 @@
+﻿using KRAFT.Results.WebApi.Abstractions;
+
+namespace KRAFT.Results.WebApi.Features.Athletes;
+
+internal sealed record class AthleteCreatedEvent(Athlete Athlete) : IDomainEvent;
---
+++ src/KRAFT.Results.WebApi/Features/Participations/ParticipationAddedEvent.cs
@@ -0,0 +1,5 @@
+﻿using KRAFT.Results.WebApi.Abstractions;
+
+namespace KRAFT.Results.WebApi.Features.Participations;
+
+internal sealed record class ParticipationAddedEvent(Participation Participation) : IDomainEvent;
