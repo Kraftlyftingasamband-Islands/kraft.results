54ece1590698e4e7924a897b2f177530d8b503bb
Kári Rafn Karlsson <karirafn@gmail.com>
2026-04-06 13:29:30 +0000

    feat(abstractions): add domain event publisher (#351)

---
+++ src/KRAFT.Results.WebApi/Abstractions/IDomainEventPublisher.cs
@@ -0,0 +1,6 @@
+﻿namespace KRAFT.Results.WebApi.Abstractions;
+
+internal interface IDomainEventPublisher
+{
+    Task PublishAsync(IDomainEvent domainEvent, CancellationToken cancellationToken = default);
+}
