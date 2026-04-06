c6053c871b00351edf2b1ee460cad15dd6619b64
Kári Rafn Karlsson <karirafn@gmail.com>
2026-04-06 13:25:20 +0000

    feat(abstractions): add domain event abstractions (#351)
    Add IDomainEvent marker interface, IDomainEventHandler<T> generic
    handler interface, and AggregateRoot base class with domain event
    collection and raising support.

---
+++ src/KRAFT.Results.WebApi/Abstractions/IDomainEventHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace KRAFT.Results.WebApi.Abstractions;
+
+internal interface IDomainEventHandler<in T>
+    where T : IDomainEvent
+{
+    Task HandleAsync(T domainEvent, CancellationToken cancellationToken);
+}
