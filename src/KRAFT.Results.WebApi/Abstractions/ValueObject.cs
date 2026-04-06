4278c29823a6c3672e8b3d369ec324e2224e172a
Kári Rafn Karlsson <karirafn@gmail.com>
2026-04-06 11:56:47 +0000

    fix(abstractions): add value equality to ValueObject<T>
    Override Equals, GetHashCode, and == / != operators using
    EqualityComparer<T>.Default. Implement IEquatable<ValueObject<T>>.
    Cross-type safety enforced via runtime type check in both Equals
    overloads. Add new KRAFT.Results.WebApi.Tests unit test project with
    equality tests covering all five concrete value objects.
    
    Closes #349

--- src/KRAFT.Results.WebApi/Abstractions/ValueObject.cs
+++ src/KRAFT.Results.WebApi/Abstractions/ValueObject.cs
@@ -1,6 +1,6 @@
 ﻿namespace KRAFT.Results.WebApi.Abstractions;
 
-internal abstract class ValueObject<T>
+internal abstract class ValueObject<T> : IEquatable<ValueObject<T>>
 {
     protected ValueObject(T value)
     {
@@ -10,6 +10,60 @@ internal abstract class ValueObject<T>
     public T Value { get; }
 
     public static implicit operator T(ValueObject<T> title) => title.Value;
+
+    public static bool operator ==(ValueObject<T>? left, ValueObject<T>? right)
+    {
+        if (left is null)
+        {
+            return right is null;
+        }
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject<T>? left, ValueObject<T>? right) => !(left == right);
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Equals((ValueObject<T>)obj);
+    }
+
+    public bool Equals(ValueObject<T>? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        if (other.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return EqualityComparer<T>.Default.Equals(Value, other.Value);
+    }
+
+    public override int GetHashCode() => EqualityComparer<T>.Default.GetHashCode(Value!);
 
     public override string ToString() => Value?.ToString() ?? string.Empty;
 }
