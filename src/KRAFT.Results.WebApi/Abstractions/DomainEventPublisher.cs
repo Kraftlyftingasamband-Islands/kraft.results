f1b7a93b55b15d513c09505e2c9dc1900cfdd1a8
Kári Rafn Karlsson <karirafn@gmail.com>
2026-04-06 13:29:30 +0000

    feat(abstractions): add domain event publisher (#351)

---
+++ src/KRAFT.Results.WebApi/Abstractions/DomainEventPublisher.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace KRAFT.Results.WebApi.Abstractions;
+
+internal sealed class DomainEventPublisher(IServiceProvider serviceProvider) : IDomainEventPublisher
+{
+    public async Task PublishAsync(IDomainEvent domainEvent, CancellationToken cancellationToken = default)
+    {
+        Type eventType = domainEvent.GetType();
+        Type handlerType = typeof(IDomainEventHandler<>).MakeGenericType(eventType);
+        IEnumerable<object?> handlers = serviceProvider.GetServices(handlerType);
+
+        foreach (object? handler in handlers)
+        {
+            if (handler is null)
+            {
+                continue;
+            }
+
+            System.Reflection.MethodInfo? method = handlerType.GetMethod("HandleAsync");
+            if (method is not null)
+            {
+                await (Task)method.Invoke(handler, [domainEvent, cancellationToken])!;
+            }
+        }
+    }
+}
