48fb4976f86860114d89ab53ea3fb6fbc65daa71
Kári Rafn Karlsson <karirafn@gmail.com>
2026-04-26 10:49:01 +0000

    style(webapi): wrap SuppressMessage attribute to fit 120-char limit

--- tests/KRAFT.Results.WebApi.IntegrationTests/Collections/GetRecordsByEraTestsCollection.cs
+++ tests/KRAFT.Results.WebApi.IntegrationTests/Collections/GetRecordsByEraTestsCollection.cs
@@ -3,7 +3,10 @@ using System.Diagnostics.CodeAnalysis;
 namespace KRAFT.Results.WebApi.IntegrationTests.Collections;
 
 [CollectionDefinition(nameof(GetRecordsByEraTestsCollection))]
-[SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "xUnit collection definition")]
+[SuppressMessage(
+    "Naming",
+    "CA1711:Identifiers should not have incorrect suffix",
+    Justification = "xUnit collection definition")]
 public sealed class GetRecordsByEraTestsCollection : ICollectionFixture<CollectionFixture>
 {
 }
